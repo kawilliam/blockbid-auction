449b2727577ce32bb890eca8e62b3a264cc6fe3a
Kyle Williamson <mgkshome7@gmail.com>
2025-11-18 16:52:37 -0500

    Deliverable2DesignUpdate

---
+++ src/main/java/com/blockbid/controller/WebController.java
@@ -0,0 +1,38 @@
+package com.blockbid.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class WebController {
+	
+	@GetMapping("/")
+	public String index() {
+		return "index";
+	}
+	
+	@GetMapping("/catalogue.html")
+	public String catalogue() {
+		return "catalogue";
+	}
+	
+	@GetMapping("/test")
+	public String test() {
+	    return "index";  // Should show index page
+	}
+	
+	@GetMapping("/bidding.html")
+	public String bidding() {
+		return "bidding";
+	}
+	
+	@GetMapping("/payment.html")
+	public String payment() {
+		return "payment";
+	}
+	
+	@GetMapping("/receipt.html")
+	public String receipt() {
+	    return "receipt"; 
+	}
+}
